f479df72b1aca4d6680144f95a43d1991704772d
JoseCaste <jotacastellanosguz@gmail.com>
2022-02-07 19:37:36 -0600

    Fix cors policy

--- ExamenOrdinario/src/server.js
+++ ExamenOrdinario/src/server.js
@@ -4,16 +4,17 @@ let express=require("express");
 const bodyParser = require("body-parser");
 let server=express();
 let router= require("./routes/articles-routes")
-
+let cors= require("cors");
 
 //Archivos/Rutas
 
 //middleware
+server.use(cors());
 server.use(bodyParser.urlencoded({extended: false}));
 server.use(bodyParser.json());
 server.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'DELETE, PUT','POST');
+    res.header('Access-Control-Allow-Methods', 'DELETE', 'PUT','POST','GET');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
    next();
   });
